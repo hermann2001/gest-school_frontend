0aa029a23795348583148ac9fcbb582f92b58342
dalpi-progs <dalpiprogs@localhost.localdomain>
2024-07-16 21:09:00 +0100

    adaptation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import Homepage from './pages/Homepage';
+import LoginPage from './pages/LoginPage';
 
-function App() {
+const App = () => {
+  const { currentRole } = useSelector(state => state.user);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      {currentRole === null &&
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+
+          <Route path="/login" element={<LoginPage />} />
+
+          <Route path='*' element={<Navigate to="/" />} />
+        </Routes>}
+
+      {/* {currentRole === "Admin" &&
+        <>
+          <AdminDashboard />
+        </>
+      } */}
+
+      {/* {currentRole === "Student" &&
+        <>
+          <StudentDashboard />
+        </>
+      }
+
+      {currentRole === "Teacher" &&
+        <>
+          <TeacherDashboard />
+        </>
+      } */}
+    </Router>
+  )
 }
 
-export default App;
+export default App
